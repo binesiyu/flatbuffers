2f8bc3b68677c39b6f0e2b6cfe7e31d8e0136cb7
yubin <1960028060@qq.com>
2022-10-18 12:05:51 +0800

    lua:fb

--- lua_fix/flatbuffers.lua
+++ lua_fix/flatbuffers.lua
@@ -7,6 +7,8 @@ m.N = _G.flatbuffersnative.N
 m.binaryArray = {}
 m.binaryArray.New = _G.flatbuffersnative.new_binaryarray
 
+m.binaryarray = m.binaryArray
+
 m.view = {}
 m.view.New = _G.flatbuffersnative.new_view
 
--- samples/sample_data_native.lua
+++ samples/sample_data_native.lua
@@ -1,19 +1,28 @@
 -- need to update the Lua path to point to the local flatbuffers implementation
 -- package.cpath = string.format("/Users/yubin/.luarocks/lib/lua/5.3/?.so;%s",package.path)
 -- package.cpath = string.format("/Users/yubin/Documents/dev/lua/lua-flatbuffers-master/external/lua-flatbuffers/?.so;%s",package.cpath)
-package.path = string.format("/Users/yubin/Documents/dev/lua/lua-flatbuffers-master/external/lua-flatbuffers/?.lua;%s",package.path)
+-- package.path = string.format("/Users/yubin/Documents/dev/lua/lua-flatbuffers-master/external/lua-flatbuffers/?.lua;%s",package.path)
+package.path = string.format("../lua_fix/?.lua;%s",package.path)
 package.path = string.format("./luafix/?.lua;%s",package.path)
 
 -- require the library
 local flatbuffers = require("flatbuffers")
+local weapon = require("MyGame.Sample.Weapon")
 local monster = require("MyGame.Sample.Monster")
+local color = require("MyGame.Sample.Color")
+local equipment = require("MyGame.Sample.Equipment")
 
 local function checkReadBuffer(buf, offset, sizePrefix)
     offset = offset or 0
 
     if type(buf) == "string" then
+        print("bufAsString",string.len(buf))
+        local s = string.gsub(buf,"(.)",function (x) return string.format("%d ",string.byte(x)) end)
+        print(s)
+
         buf = flatbuffers.binaryArray.New(buf)
     end
+
 
     if sizePrefix then
         local size = flatbuffers.N.Int32:Unpack(buf, offset)
@@ -28,6 +37,23 @@ local function checkReadBuffer(buf, offset, sizePrefix)
     assert(mon.pos.x == 10.0)
     assert(mon.pos.y == 2.0)
     assert(mon.pos.z == 3.0)
+
+    local expected = {
+        {w = 'axe', d = 100},
+        {w = 'bow', d = 90}
+    }
+
+    for i=1,mon.weaponsLength do
+       assert(mon.weapons[i].name == expected[i].w)
+       assert(mon.weapons[i].damage == expected[i].d)
+    end
+
+    assert(mon.equipped_type == equipment.Weapon)
+
+    local unionWeapon = weapon.New()
+    unionWeapon(mon.equipped.bytes,mon.equipped.pos)
+    assert(unionWeapon.name == "bow")
+    assert(unionWeapon.damage == 90)
 end
 
 local function testCanonicalData()
